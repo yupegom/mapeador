be227400502d6490dfa781a21bc9777051707de9
Yuliban <yupegom@gmail.com>
2016-11-17 08:42:04 -0500

    Parseador refactor

--- src/main/scala/co/com/ach/soi/data/Parseadores.scala
+++ src/main/scala/co/com/ach/soi/data/Parseadores.scala
@@ -1,7 +1,5 @@
 package co.com.ach.soi.data
 
-import shapeless.{ ::, Generic, HList, HNil }
-
 import scala.util.Try
 
 trait Parseador[A] {
@@ -21,26 +19,5 @@ object Parseadores {
   implicit val doubleParseador: Parseador[Double] = new Parseador[Double] {
     def apply(s: String): Option[Double] = Try(s.toDouble).toOption
   }
-
-  implicit val hnilParser: Parseador[HNil] = new Parseador[HNil] {
-    def apply(s: String): Option[HNil] = if (s.isEmpty) Some(HNil) else None
-  }
-
-  implicit def hconsParseador[H: Parseador, T <: HList: Parseador]: Parseador[H :: T] = new Parseador[H :: T] {
-    def apply(s: String): Option[H :: T] = s.split(",").toList match {
-      case cell +: rest => for {
-        head <- implicitly[Parseador[H]].apply(cell)
-        tail <- implicitly[Parseador[T]].apply(rest.mkString(","))
-      } yield head :: tail
-    }
-  }
-
-  implicit def caseClassParser[A, R <: HList](implicit
-    gen: Generic[A] { type Repr = R },
-    reprParser: Parseador[R]): Parseador[A] = new Parseador[A] {
-    def apply(s: String): Option[A] = reprParser.apply(s).map(gen.from)
-  }
-
-  def apply[A](s: String)(implicit parser: Parseador[A]): Option[A] = parser(s)
 
 }
--- src/test/scala/co/com/ach/soi/data/FilaParseadorSpec.scala
+++ src/test/scala/co/com/ach/soi/data/FilaParseadorSpec.scala
@@ -7,13 +7,13 @@ class FilaParseadorSpec extends Matchers with WordSpecLike {
 
   "A file parser " should {
     "parsear" in {
-      val person = Parseadores[Person]("Amy, 54.2")
+      val person = LineaParseador[Person]("Amy, 54.2")
       person.get.name should be("Amy")
 
     }
 
     "parsear clase grande " in {
-      val c = Parseadores[EncabezadoEntrada2145]("1,2,3,3,3,3,3,3,3,3,3,3,3,1,1.0,A,B")
+      val c = LineaParseador[EncabezadoEntrada2145]("1,2,3,3,3,3,3,3,3,3,3,3,3,1,1.0,A,B")
       c.get.codigoOperador should be("B")
     }
   }
